90f5977a466ef356bb69f0ba0feb2b408f0f81d5
AdilRashidov <42243485+AdilRashidov@users.noreply.github.com>
2018-10-26 14:59:50 +0600

    DTO default value

--- ToDoApp/Automapper/ToDoDTO.cs
+++ ToDoApp/Automapper/ToDoDTO.cs
@@ -8,7 +8,7 @@ namespace ToDoApp.Automapper
     public class ToDoDTO
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
         public string Tags { get; set; }
         public string Desc { get; set; }
         public bool Check { get; set; }
